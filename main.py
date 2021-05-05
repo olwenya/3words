70ffac42a89717af5e4ca29f99cc5a4cdd950375
Allan Olweny <allan.olweny@gmail.com>
2021-05-05 17:02:37 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+with open('words.txt','r') as file:
+  lines = file.readlines()
+
+  print(lines)
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer()
+train = cv.fit_transform(lines)
+
+import numpy as np
+zipped = zip(cv.get_feature_names(), np.ravel(train.sum(axis=0)))
+
+import csv
+
+with open('output.csv', "w") as output:
+        writer = csv.writer(output)
+        writer.writerow(['word','count'])
+        zip = [l for l in zipped]
+        for l in zip:
+            writer.writerow(l)
+        print('Successful grab')
